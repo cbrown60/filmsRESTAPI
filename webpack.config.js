797eb67d4ef627bae682f93a2c004b1377bdff65
Chris <cbrown605@googlemail.com>
2017-04-17 16:18:39 +0100

    files set up

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+var config = {
+  //define a key 
+  entry:'./client/src/app.js',
+  output:{
+      filename: 'bundle.js',
+      path:'./client/build'
+  },
+  devtool: 'source-map'
+}
+
+
+module.exports = config 
